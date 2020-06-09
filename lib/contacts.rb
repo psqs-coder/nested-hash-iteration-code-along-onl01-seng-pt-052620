98df7db303e3c519b1e699ff595b13fc7652c053
Bruce Parker <detailsharks@gmail.com>
2020-06-09 01:58:58 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -8,3 +8,15 @@ def remove_strawberry(contacts)
   end
 end
 
+def remove_strawberry(contacts)
+  # Assign Freddy Mercury's hash to a variable
+  freddy = contacts["Freddy Mercury"]
+
+  # Iterate over only Freddy Mercury's data
+  freddy.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if{ |ice_cream| ice_cream == "strawberry" }
+    end
+  end
+  contacts
+end 
